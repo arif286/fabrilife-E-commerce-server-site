696454295cdcc3deabaed71646848be79645739e
arif286 <stunnedarif@gmail.com>
2021-03-31 02:20:48 +0600

    server

---
+++ .history/index_20210331001821.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mnfgc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const eventCollection = client.db("tshirtShop").collection("products");
+
+  app.get("/event", (req, res) => {
+    eventCollection.find().toArray((err, items) => {
+      res.send(items);
+      console.log(items);
+    });
+  });
+  app.post("/addEvent", (req, res) => {
+    const newEvent = req.body;
+    eventCollection.insertOne(newEvent.data).then((result) => {
+      console.log(result);
+      res.send(result.insertedCount > 0);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# full-stack-server-arif286
